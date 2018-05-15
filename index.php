b1de7ae665c7ea592eab2316659e5b5a75046a31
Gabriela <Gabriela@VAIO>
2018-05-15 01:17:06 -0300

    inserido no projeto algumas imagens para após utilizar no código/ colocado href

--- index.php
+++ index.php
@@ -8,8 +8,35 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>Página Inicial</title>
+        <img src= "Logo_Nova2.jpg">
     </head>
     <body>
+       
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+						<li >
+							<a href="empresa.php">EMPRESA</a>
+						</li>
+						<li>
+							<a href="pecas.php">PEÇAS</a>
+						</li>
+						<li>
+							<a href="servicos.">SERVIÇOS</a>
+						</li>
+						<li>
+							<a href="contato.php">CONTATO</a>
+						</li>
+                                                <li>
+							<a href="Administrador.php">ADMINISTRADOR</a>
+						</li>
+
+							</ul>
+						</li>
+					</ul>
+				
+					
+				</div>	
+			</nav>
         <?php
         // put your code here
         ?>
