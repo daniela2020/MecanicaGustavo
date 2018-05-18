81430bff2df51b8da37a3eae615ec85af80a74b5
Gabriela <Gabriela@VAIO>
2018-05-18 18:35:47 -0300

    feito o menu

--- index.php
+++ index.php
@@ -1,44 +1,69 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Página Inicial</title>
-        <img src= "Logo_Nova2.jpg">
-    </head>
-    <body>
-       
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-						<li >
-							<a href="empresa.php">EMPRESA</a>
-						</li>
-						<li>
-							<a href="pecas.php">PEÇAS</a>
-						</li>
-						<li>
-							<a href="servicos.">SERVIÇOS</a>
-						</li>
-						<li>
-							<a href="contato.php">CONTATO</a>
-						</li>
-                                                <li>
-							<a href="Administrador.php">ADMINISTRADOR</a>
-						</li>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- As 3 metatags acima * devem * vir em primeiro lugar na cabeça;
+    qualquer outro conteúdo principal deve vir * depois * dessas tags -->
+    <title>Mecânica Gustavo</title>
 
-							</ul>
-						</li>
-					</ul>
-				
-					
-				</div>	
-			</nav>
-        <?php
-        // put your code here
-        ?>
-    </body>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+	  <a class="navbar-brand" href="#">Home</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="#">Empresa</a></li>
+        <li class=><a href="#">Peças</a></li>
+        <li><a href="#">Contato</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Serviços <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Motor</a></li>
+            <li><a href="#">Caixa de Câmbio</a></li>
+			<li><a href="#">Diferencial</a></li>
+            <li><a href="#">Direção Hidráulica</a></li>
+			<li><a href="#">Suspensão e Freios</a></li>
+			<li><a href="#">Reparos Mecânicos</a></li>
+            
+            
+           
+          </ul>
+        </li>
+      </ul>
+      
+        </div>
+        
+      
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
 </html>
