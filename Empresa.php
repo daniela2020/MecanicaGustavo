2cf1d43eb1e521be5e3db0f1aad2dbfb9223ce5b
daniela2020 <danielasampaionatividade@gmail.com>
2018-05-21 20:27:24 -0300

    histórico da empresa

--- Empresa.php
+++ Empresa.php
@@ -1,8 +1,102 @@
+!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <link rel="icon" href="favicon.ico">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- As 3 metatags acima * devem * vir em primeiro lugar na cabeça;
+    qualquer outro conteúdo principal deve vir * depois * dessas tags -->
+    <title>Mecânica Gustavo  </title>
+        <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+      <header><h1>Mecânica Gustavo</h1></header>
+      <img src=Logo_Nova2.jpg>
+    <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+	  <a class="navbar-brand" href="index.php">Home</a>
+         
+          
+    </div>
+    
+
+    <!-- Colete os links de navegação, formulários e outros conteúdos para alternar -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+          <li><a href="Empresa.php">Empresa</a></li>
+          <li class=><a href="Pecas.php">Peças</a></li>
+          <li><a href="Contato.php">Contato</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Serviços <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Motor</a></li>
+            <li><a href="#">Caixa de Câmbio</a></li>
+			<li><a href="#">Diferencial</a></li>
+            <li><a href="#">Direção Hidráulica</a></li>
+			<li><a href="#">Suspensão e Freios</a></li>
+			<li><a href="#">Reparos Mecânicos</a></li>
+            
+            
+           
+          </ul>
+        </li>
+      </ul>
+    </div>
+
+
+
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
+
+echo  "A Mecânica Gustavo, empresa localizada à Av Ipiranga, 536 – Bairro Kurtz, nesta cidade, atua no mercado desde 27/08/1985, no ramo de Peças e Prestação de Serviços de Caminhões e Camionetas da linha diesel e também na parte hidráulica de veículos da linha leve.<br>";
+echo  "Desde a sua criação, busca superar obstáculos, comercializando produtos diversificados de qualidade e serviços especializados, com comprometimento, competitividade e qualidade, no intuito de garantir a satisfação do cliente.<br>";
+echo  "		Prima por princípios fundamentais para atingir seus objetivos:<br>";
+echo  "	HONESTIDADE<br>";
+echo  " RESPEITO<br>";
+echo  "	ÉTICA <br>";
+echo  "	COMPETÊNCIA<br>";
+echo  "	AGILIDADE<br>";
+
+echo "POLÍTICA DA EMPRESA";
+
+echo " Nossa política de relacionamento está pautada em aspectos essenciais:<br>";
+echo "► Excelência no atendimento, buscando satisfazer as expectativas dos clientes<br>";
+echo "► Transparência nas relações com o cliente;<br>";
+echo "► Melhoria contínua da qualidade dos serviços, investindo em equipamentos e ferramentas de última geração<br>"; 
+echo "► Treinamentos e Capacitação dos Funcionários, com foco nos resultados<br>";
+echo "►Controle dos impactos ambientais e riscos ocupacionais.<br>";
+
+
+
+echo "<h1>MISSÃO</h1><br>";
+echo "BUSCA CONSTANTE DE APRIMORAMENTO, NOS SERVIÇOS PRESTADOS E FORTALECIMENTO NAS RELAÇÕES COM CLIENTES E FORNECEDORES<br>";
+
+echo "<h1>VISÃO</h1><br>";
+
+echo "“OBJETIVA MANTER-SE NA LIDERANÇA DO SEGMENTO DE VENDAS DE PEÇAS E PRESTAÇÃO DE SERVIÇOS, PROPORCIONANDO A SATISFAÇÃO DOS CLIENTES.”<br>";
+
+echo "<h1>VALORES</h1>";
+
+echo "ATUAR POSITIVAMENTE NO MERCADO, COM ÉTICA, RESPEITO E CONFIABILIDADE.”<br>;
+ <br>";
+
 
