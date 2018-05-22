1e29ad495a16ae46f5538b0d485bd1535ee18c90
daniela2020 <danielasampaionatividade@gmail.com>
2018-05-22 19:55:40 -0300

    vspace - define o tamanho da margem nas bordas superior e inferior da imagem. hspace - define o tamanho da margem nas bordas esquerda e direita da imagem.

--- index.php
+++ index.php
@@ -3,18 +3,24 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Página Inicial</title>
+        <title>Mecânica Gustavo</title>
         <link rel="stylesheet" type="text/css"  href="estilo.css" />
         <img src= "Logo_Nova2.jpg">
     </head>
     <body>
-       
+       vspace - define o tamanho da margem nas bordas superior e inferior da imagem.
+       hspace - define o tamanho da margem nas bordas esquerda e direita da imagem.
+
+
+        <img src="imagens/Logo_Nova2.jpg" alt="Minha imagem!" vspace="10px" hspace="5px" border="5px" align="left"/>
         
+
+
         <ul class="menu">
             
             <nav>
   <ul class="menu">
-      <li><a href="index.php">Home</a></li>
+      <li><a href="index.php">HOME</a></li>
                       
 
 						<li >
@@ -22,7 +28,7 @@
 						<li>
 							<a href="pecas.php">PEÇAS</a></li>
 						<li>
-							<a href="servicos.">SERVIÇOS</a></li>
+							<a href="servicos.php">SERVIÇOS</a></li>
                                                 
 						<li>
 							<a href="contato.php">CONTATO</a></li>
