9913505879828d904496550031de3f1c00cf8168
ronaldogaucho <ronaldozgaucho04@gmail.com>
2018-05-22 22:18:26 -0300

    Alteraçoes em conjunto.

--- index.php
+++ index.php
@@ -1,51 +1,49 @@
 <!DOCTYPE html>
-
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Mecânica Gustavo</title>
         <link rel="stylesheet" type="text/css"  href="estilo.css" />
-        <img src= "Logo_Nova2.jpg">
+
     </head>
     <body>
-       vspace - define o tamanho da margem nas bordas superior e inferior da imagem.
-       hspace - define o tamanho da margem nas bordas esquerda e direita da imagem.
+        <!-- vspace - define o tamanho da margem nas bordas superior e inferior da imagem.
+        hspace - define o tamanho da margem nas bordas esquerda e direita da imagem. -->
+        <!-- <img src=Logo_Nova2.jpg> Imagem pequena do logo-->
+
+        <img src="imagens/logo.png" vspace="10px" hspace="5px" border="5px" align="left"/>
 
 
-       <img src="imagens/logo.png" alt="Minha imagem!" vspace="10px" hspace="5px" border="5px" align="left"/>
-        
+           <ul class="menu">
+                
+                <li><a href="index.php">HOME</a></li>
 
+                <li><a href="empresa.php">EMPRESA</a></li>
+                    
+                <li><a href="pecas.php">PEÇAS</a></li>
+                                       
+                <li><a href="contato.php">CONTATO</a></li>
 
-        <ul class="menu">
+                <li><a href="Administrador.php">ADMINISTRADOR</a></li>
+
+                <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Serviços <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+              <li><a href="Servicos.php">Motor</a></li>
+              <li><a href="Servicos.php">Caixa de Câmbio</a></li>
+              <li><a href="Servicos.php">Diferencial</a></li>
+              <li><a href="Servicos.php">Direção Hidráulica</a></li>
+              <li><a href="Servicos.php">Suspensão e Freios</a></li>
+              <li><a href="Servicos.php">Reparos Mecânicos</a></li>
             
-            <nav>
-  <ul class="menu">
-      <li><a href="index.php">HOME</a></li>
-                      
+            
+           
+            </ul>
 
-						<li >
-							<a href="empresa.php">EMPRESA</a></li>
-						<li>
-							<a href="pecas.php">PEÇAS</a></li>
-						<li>
-							<a href="servicos.php">SERVIÇOS</a></li>
-                                                
-						<li>
-							<a href="contato.php">CONTATO</a></li>
-                                                <li>
-							<a href="Administrador.php">ADMINISTRADOR</a></li>
 
-							</ul>
-						</li>
-					</ul>
-				
-					
-				</div>	
-			</nav>
         <?php
         // put your code here
         ?>
-            <header><h1>Mecânica Gustavo</h1></header>
-      <img src=Logo_Nova2.jpg>
+
     </body>
 </html>
